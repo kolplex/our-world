abf255fd0e5a0acce1b7bcd6cd3dd511e66b0bef
Ahmet Koldaguc <ahmetkoldaguq@gmail.com>
2026-06-24 15:45:05 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-tet
+let ideas = [
+"Watch a movie together 🎬",
+"Play Terraria together 🎮",
+"Listen to music together 🎵"
+];
+
+function newIdea() {
+    let random = Math.floor(Math.random() * ideas.length);
+    document.getElementById("idea").innerText = ideas[random];
+}
+
+let movies = [
+"Coraline",
+"Interstellar",
+"Nosferatu"
+];
+
+function randomMovie() {
+    let random = Math.floor(Math.random() * movies.length);
+    document.getElementById("movie").innerText = movies[random];
+}
+
+function saveNote() {
+    ...
+}
